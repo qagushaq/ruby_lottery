b70a64f9befc7c0ea702d6250f7973841ac13069
qagushaq <qwerty24164444@gmail.com>
2019-09-02 21:02:37 +0300

    games without restarting(ref) and  checker choose

--- lottery.rb
+++ lottery.rb
@@ -1,88 +1,6 @@
 def rsc_bot
 [:камень,:ножницы,:бумагу][rand(0..2)]
 end
-def game4(money)
-	if money<=0
-		puts "Вы банкрот:(\nСпасибо за игру:)"
-		exit
-		else
-		arr = [:камень,:ножницы,:бумагу]
-		while true
-			puts "1-камень, 2-ножницы,3-бумага"
-			cmd=gets.chomp.strip.to_i
-			 #после введения цифры, отдельно вынести всю прогу в метод, чтобы он вызывался if cmd>3 введи нормально else запустить всю прогу еще раз
-			user=arr[cmd-1]
-			bot = rsc_bot
-			puts "Вы выбрали #{user}"
-			puts "Бот выбрал #{bot}"
-			if user==bot
-				puts "Draw. Ваш баланс:#{money}" #добавить стоимость игры для всех модулей
-			elsif user ==:камень && bot==:ножницы
-				money+=1
-				puts "You win! Ножницы сломались об камень! Ваш баланс:#{money}"
-			elsif user ==:камень && bot==:бумагу
-				money-=1
-				puts "You lose. Бумага накрыла камень. Ваш баланс:#{money}"
-			elsif user ==:ножницы && bot==:камень
-				money-=1
-				puts "You lose. Ножницы сломались об камень. Ваш баланс:#{money}"
-			elsif user ==:ножницы && bot==:бумагу
-				money+=1
-				puts "You win!Ножницы разрезали бумагу! Ваш баланс:#{money}"
-			elsif user ==:бумагу  && rsc_bot==:камень
-				money+=1
-				puts "You win! Бумага накрыла камень! Ваш баланс:#{money}"
-			elsif user ==:бумагу && rsc_bot==:ножницы
-				money-=1
-				puts "You lose. Ножницы разрезали бумагу. Ваш баланс:#{money}"
-			end
-		end
-	end
-end
-
-def game3(money)
-  print "Привет! Загадываю число от 1 до "
-  n=gets.chomp.strip.to_i
-  if  n<=50
-      price = 10
-  elsif n>51 && n<=100	#придумать динамический алгоритм,чтобы вычислять стоимость игры
-    price = 11 # исходя из 1..n по кол-ву попыток для ответа 50-5 100-7 +-
-  end
-  puts "Цена одной попытки #{price}$. Удачи!"
-  number = rand(1..n)
-	1.upto(n) do |i|
-    puts "Я загадал число от 1 до #{n}, угадай какое. Попытка #{i}:"
-    x = gets.chomp.strip.to_i
-    if x == number
-			money+=50
-      puts "Ты угадал, число #{number}. Твой баланс:#{money}"
-      break
-    elsif x < number
-			money-=price
-		  puts "Нет,больше. Твой баланс:#{money}"
-    elsif x > number
-			money-=price
-      puts "Нет,меньше. Твой баланс:#{money}"
-    end
-  end #ПЕРЕДЕЛАТЬ ПОКА ТРУ И ДЕНЬГИ ЕСТЬ МОЖНО УГАДЫВАТЬ  УБРАТЬ try
-end
-
-def game1(money)
-  print "Цена одного билета 2$.Сколько раз будем играть:"
-  count_of_ticket = gets.chomp.strip.to_i
-  money-=count_of_ticket*2
-  puts "Куплено #{count_of_ticket} билетов, у вас осталось #{money}$"
-  print "Сколько будет билетов:"
-  count_all_ticket = gets.chomp.strip.to_i
-	prize = count_all_ticket/3
-  1.upto(count_of_ticket) do |i|
-    puts "Играем #{i} раз шанс равен 1:#{(count_all_ticket.to_f/i.to_f).round(1)}" #сделать шанс в виде 1 к 7.2 1 к 5.4    1/10 = 0.1  #(i.to_f/count_all_ticket.to_f) - шанс 0.1 0.89 и тд
-    if i == rand(i..count_all_ticket)
-      money+=prize
-      puts "Вы выиграли #{prize}$, Ваш баланс #{money}$. Мои поздравления! Понадобилось всего #{i} попыток"
-    end
-  end
-end
 
 def rnd_comment(money,m_money)
   comment = rand (1..5)
@@ -98,6 +16,34 @@ def rnd_comment(money,m_money)
       puts "Минус #{m_money}$. Эти деньги пойдут на благотворительность!.Осталось #{money}.$"
   end
 end
+
+def checker_limit_number(input,max_number)
+	if input.to_i > max_number.to_i
+		puts "Введите существующий вариант: от 1 до #{max_number}"
+	else
+		return true
+	end
+end#обернуть все в вайл тру и выскакивать к нему, когда 5>4
+
+def game1(money)#добавить проверку на достаточное кол-во денег
+print "Цена одного билета 2$ у вас #{money}$.Сколько раз будем играть:"
+count_of_ticket = gets.chomp.strip.to_i
+  money-=count_of_ticket*2
+  puts "Куплено #{count_of_ticket} билетов, у вас осталось #{money}$"
+  print "Сколько будет билетов:"
+  count_all_ticket = gets.chomp.strip.to_i
+	prize = count_all_ticket/3
+  1.upto(count_of_ticket) do |i|
+    puts "Играем #{i} раз шанс равен 1:#{(count_all_ticket.to_f/i.to_f).round(1)}"
+    if i == rand(i..count_all_ticket)
+      money+=prize
+      puts "Вы выиграли #{prize}$, Ваш баланс #{money}$. Мои поздравления! Понадобилось всего #{i} попыток"
+    end
+  end
+	return money
+end
+
+
 
 def game2(money)
   while true do
@@ -145,27 +91,103 @@ def game2(money)
           end
       end
   end
+	return money
+end
+
+def game3(money)
+  print "Привет! Загадываю число от 1 до "
+  n=gets.chomp.strip.to_i
+  if  n<=50
+      price = 10
+  elsif n>51 && n<=100	#придумать динамический алгоритм,чтобы вычислять стоимость игры
+    price = 11 # исходя из 1..n по кол-ву попыток для ответа 50-5 100-7 +-
+  end
+  puts "Цена одной попытки #{price}$. Удачи!"
+  number = rand(1..n)
+	1.upto(n) do |i|
+    puts "Я загадал число от 1 до #{n}, угадай какое. Попытка #{i}:"
+    x = gets.chomp.strip.to_i
+    if x == number
+			money+=50
+      puts "Ты угадал, число #{number}. Твой баланс:#{money}"
+      break
+    elsif x < number
+			money-=price
+		  puts "Нет,больше. Твой баланс:#{money}"
+    elsif x > number
+			money-=price
+      puts "Нет,меньше. Твой баланс:#{money}"
+    end
+		break if money<10
+  end
+	return money
+end
+
+def game4(money)
+	if money<=0
+		puts "Вы банкрот:(\nСпасибо за игру:)"
+		exit
+		else
+		arr = [:камень,:ножницы,:бумагу]
+		while money>=1
+			puts "1-камень, 2-ножницы,3-бумага"
+			cmd=gets.chomp.strip.to_i
+
+			#break if cmd>3
+			##########
+			user=arr[cmd-1]
+			bot = rsc_bot
+			puts "Вы выбрали #{user}"
+			puts "Бот выбрал #{bot}"
+			if user==bot
+				puts "Draw. Ваш баланс:#{money}" #добавить стоимость игры для всех модулей
+			elsif user ==:камень && bot==:ножницы
+				money+=1
+				puts "You win! Ножницы сломались об камень! Ваш баланс:#{money}"
+			elsif user ==:камень && bot==:бумагу
+				money-=1
+				puts "You lose. Бумага накрыла камень. Ваш баланс:#{money}"
+			elsif user ==:ножницы && bot==:камень
+				money-=1
+				puts "You lose. Ножницы сломались об камень. Ваш баланс:#{money}"
+			elsif user ==:ножницы && bot==:бумагу
+				money+=1
+				puts "You win!Ножницы разрезали бумагу! Ваш баланс:#{money}"
+			elsif user ==:бумагу  && bot==:камень
+				money+=1
+				puts "You win! Бумага накрыла камень! Ваш баланс:#{money}"
+			elsif user ==:бумагу && bot==:ножницы
+				money-=1
+				puts "You lose. Ножницы разрезали бумагу. Ваш баланс:#{money}"
+			end
+			break if checker_limit_number(cmd,3) == false###
+		end
+	end
+	return money
 end
 
 def choose_game(money)
-  puts "Во что будем играть ?"
-  puts "Лотерея-1"#сделать все цифры на одном уровне
-  puts "Однорукий-бандит - 2"
-  puts "Угадай число - 3"
-  puts "Камень Ножницы Бумага - 4"
-  choose = gets.chomp.strip.capitalize
-  if choose == "1"
-    game1(money)
-  end
-  if choose == "2"
-    game2(money)
-  end
-  if choose == "3"
-    game3(money)
-  end
-  if choose =="4"
-    game4(money)
-  end
+	while true do
+	  puts "Во что будем играть ?"
+	  puts "Лотерея-1"#сделать все цифры на одном уровне
+	  puts "Однорукий-бандит - 2"
+	  puts "Угадай число - 3"
+	  puts "Камень Ножницы Бумага - 4"
+	  choose = gets.chomp.strip.capitalize
+	  if choose == "1"
+	    game1(money)
+	  end
+	  if choose == "2"
+	    game2(money)
+	  end
+	  if choose == "3"
+	    game3(money)
+	  end
+	  if choose =="4"
+	    game4(money)
+		end
+		break if checker_limit_number(choose,4) == false
+	end
 end
 
 def check_confirm
